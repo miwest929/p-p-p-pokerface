0563a8577d846a918d30c95f625f3725eb381b79
Michael West <mwest@mdsol.com>
2014-07-22 21:14:26 -0400

    Make some poker functions pass

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,22 +1,34 @@
 (ns p-p-p-pokerface)
 
+(def face-cards {\T 10, \J 11, \Q 12, \K 13, \A 14})
 (defn rank [card]
-  nil)
+  (let [[r _] card]
+  (if (Character/isDigit r) (Integer/valueOf (str r)) (face-cards r))))
 
 (defn suit [card]
-  nil)
+  (let [[_ s] card] (str s)))
 
 (defn pair? [hand]
-  nil)
+  (let [freq (vals (frequencies (map rank hand)))
+        freqCount (fn [c] (count (filter (fn [f] (= c f)) freq)))]
+  (and (= 1 (freqCount 2)) (= 3 (freqCount 1)))))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (let [freq (vals (frequencies (map rank hand)))
+        freqCount (fn [c] (count (filter (fn [f] (= c f)) freq)))]
+  (and (= 1 (freqCount 3)) (= 2 (freqCount 1)))
+))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (let [freq (vals (frequencies (map rank hand)))
+        freqCount (fn [c] (count (filter (fn [f] (= c f)) freq)))]
+  (= 1 (freqCount 4))
+))
 
 (defn flush? [hand]
-  nil)
+  (let [suits (map suit hand)]
+  (= 1 (count (set suits)))
+))
 
 (defn full-house? [hand]
   nil)
@@ -25,10 +37,13 @@
   nil)
 
 (defn straight? [hand]
-  nil)
+  (let [ranks (map rank hand)]
+  (apply < (sort ranks))
+))
 
 (defn straight-flush? [hand]
-  nil)
+ (and (straight? hand) (flush? hand))
+)
 
 (defn value [hand]
   nil)
